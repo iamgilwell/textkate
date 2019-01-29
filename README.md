eb54fd81a45a87a806b4a42ffced8025545569ac
Gilwell Muhati <rivabubusiness@gmail.com>
2019-01-29 17:58:04 +0300

    The Begining of TextKaty

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # textkate
 A cool font animation pyfiglet,users can type their names and select a font and it outputs in a fancy design
+
+virtualenv -p python3 venv
---
+++ textkate.py
@@ -0,0 +1,5 @@
+import  pyfiglet
+result1=pyfiglet.figlet_format("GILWELL'S BIRTHDAY",font="banner3-D")
+
+result=pyfiglet.figlet_format("IAM   GILWELL",font="banner3-D")
+print(result1,result)
